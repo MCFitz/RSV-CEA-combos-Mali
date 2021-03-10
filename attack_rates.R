38aba5f5baedfaec160a493402e5a88908c92a34
RachelLaufer <52212802+RachelLaufer@users.noreply.github.com>
2021-03-10 14:54:33 -0500

    update attack rates for 18 months of follow-up time

--- attack_rates.R
+++ attack_rates.R
@@ -1,8 +1,8 @@
 #########################################
 ## build AR matrix in R
 
-ft <- 23   # 1 year follow-up time
-empty_year_cohort <-  matrix (0, 12, ncol = ft)# extrapolate age-based attack rates out to 12 months, for 6-12 months project linear decline starting with AR at 6mo. and ending with AR at3mo.
+ft <- 29   # 1 year follow-up time
+empty_year_cohort <-  matrix (0, 12, ncol = ft) # extrapolate age-based attack rates out to 18 months, for 6-18 months project linear decline starting with AR at 6mo. and ending with AR at3mo.
 
 # incidence rate infant RSV cases in Mali per 1000 person-years, divided by 12 for rate in a single month
 mali_inc <- 536.8/12
@@ -24,7 +24,8 @@ age_dat <- data.frame(age = seq(1,6, by =1), cases = c(8,11,28,27,47,32), follow
 age_AR <- age_dat$cases / (age_dat$follow_up*12)
 
 ### create AR matrices for birth cohorts followed for one-year
-age_AR_y <- c(age_AR, seq(age_AR[6], age_AR[3], length = 6))
+# age_AR_y <- c(age_AR, seq(age_AR[6], age_AR[3], length = 6))
+age_AR_y <- c(age_AR, seq(age_AR[6], age_AR[3], length = 12))
 
 AR_age_weights <- age_AR_y/ (sum(age_AR_y))
 
@@ -33,8 +34,8 @@ for (a in 1:12) {
   temp3[a,] <- c(rep(0, times = a-1), age_AR_y, rep(0, times = 12-a))
 }
 
-cal_AR_y <- c(cal_AR, cal_AR[1:11])
-temp4 <- matrix(rep(cal_AR_y, each = 12), 12, 23)
+cal_AR_y <- c(cal_AR, cal_AR, cal_AR[1:5])
+temp4 <- matrix(rep(cal_AR_y, each = 12), 12, ft)
 
 AR_y_bc <- temp4/(mali_inc/1000)*temp3
 
@@ -43,16 +44,16 @@ AR_age_sample <- function (cases, fu, num_tri) {
   ARvec <- rbeta(num_tri, cases, fu*12 - cases)
 }
 
-AR_cases_year <- c(age_dat$cases, seq(age_dat$cases[6], age_dat$cases[3], length = 6))
-AR_ft_year <- c(age_dat$follow_up, seq(age_dat$follow_up[6], age_dat$follow_up[3], length =6))
+AR_cases_year <- c(age_dat$cases, seq(age_dat$cases[6], age_dat$cases[3], length = 12))
+AR_ft_year <- c(age_dat$follow_up, seq(age_dat$follow_up[6], age_dat$follow_up[3], length =12))
 AR_age_y_u <- mapply(AR_age_sample, AR_cases_year, AR_ft_year, trials)
 
-temp3_u <- array(0, dim = c(12, 23, trials))
+temp3_u <- array(0, dim = c(12, ft, trials))
 for (a in 1:12) {
-  temp3_u[a,a:(a+11),] <- t(AR_age_y_u)
+  temp3_u[a,a:(a+(ft-12)),] <- t(AR_age_y_u)
 }
 
-temp4_u <- array(rep(rep(cal_AR_y, each = 12), trials), dim = c(12, 23, trials))
+temp4_u <- array(rep(rep(cal_AR_y, each = 12), trials), dim = c(12, ft, trials))
 AR_y_u <- temp4_u/(mali_inc/1000)*temp3_u
 
 #####
